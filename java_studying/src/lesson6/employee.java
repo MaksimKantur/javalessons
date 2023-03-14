cab209cef1a0cfaa535ebd6016d349696f4c2ac2
Maksim Kantur <maxim.kantur@hotmail.com>
2023-03-14 23:13:53 +0200

    added

--- java_studying/src/lesson6/employee.java
+++ java_studying/src/lesson6/employee.java
@@ -4,8 +4,40 @@ public class employee {
     private double salary;
     public String surname;
     int id;
-    public employee(){
+    public double employeeSalary(){
+        double result = salary;
+        return result;
         
-        System.out.println();
+    }
+    public String employeeSurname(){
+        String result = surname;
+        return result;
+    }
+    public int employeeId(){
+        int result = id;
+        return result;
+    }
+    public employee(String surname1){
+    surname = surname1;
+    }
+    employee(int id1){
+        id = id1;
+        }
+    private employee(double salary1){
+    salary = salary1;
+    }
+    public static void main(String[] args) {
+        employee emp3 = new employee(500.50);
+        System.out.println(emp3.salary);
+    }
+}
+
+class employeeTest{
+    public static void main(String[] args) {    
+    employee emp1 = new employee(1);
+    employee emp2 = new employee("Stasy");
+    //employee emp3 = new employee(500.50);
+    System.out.println(emp1.id);
+    System.out.println(emp2.surname);
     }
 }
---
+++ java_studying/src/lesson6/lesson6_1/emptest.java
@@ -0,0 +1,8 @@
+package lesson6.lesson6_1;
+
+public class emptest {
+    public static void main(String[] args) {
+        lesson6.employee emp  = new lesson6.employee("John Doe");
+        System.out.println(emp.surname);
+    }
+}
